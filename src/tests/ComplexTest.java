143db0dc201ac47a2d3c90963a9b7bc8f04851b5
r1nb0 <ilia.mesha12@gmail.com>
2024-09-09 22:45:39 +0300

    added tests for complex class

--- src/tests/ComplexTest.java
+++ src/tests/ComplexTest.java
@@ -1,4 +1,33 @@
+package tests;
+
+import demo.parallel.Complex;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ComplexTest {
-  
+
+    @Test
+    public void testMinus() {
+        Complex a = new Complex(1, 2);
+        Complex b = new Complex(3, 4);
+        Complex c = a.minus(b);
+        assertEquals(new Complex(-2, -2), c);
+    }
+
+    @Test
+    public void testPlusRealPart() {
+        Complex a = new Complex(1, 2);
+        double b = 5;
+        Complex c = a.plusRealPart(b);
+        assertEquals(new Complex(6, 2), c);
+    }
+
+    @Test
+    public void testMinusRealPart() {
+        Complex a = new Complex(1, 2);
+        double b = 1;
+        Complex c = a.minusRealPart(b);
+        assertEquals(new Complex(0, 2), c);
+    }
 }
